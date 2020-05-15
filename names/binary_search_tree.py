557265ea7d7b638b7d55f4f631faaf5a586fa1a5
Phong Yang <phongyang88@hotmail.com>
2020-05-14 21:35:34 -0500

    finished names

---
+++ names/binary_search_tree.py
@@ -0,0 +1,168 @@
+"""
+Binary search trees are a data structure that enforce an ordering over 
+the data they store. That ordering in turn makes it a lot more efficient 
+at searching for a particular piece of data in the tree. 
+
+This part of the project comprises two days:
+1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
+   on the BSTNode class.
+2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
+   on the BSTNode class.
+"""
+from collections import deque
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # when we start searching, self will be the root
+        # compare the target against self
+        #
+        # Criteria for returning false: we know we need to go in one direction
+        # but there is nothing in left or right direction
+        if target == self.value:
+            return True
+        if target < self.value:
+            # go left if left is a BSTNode
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            # go right if right is a BSTNode
+            if not self.right:
+                return False
+            return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        max_value = self.value
+        if self.right:
+            max_value = self.right.value
+            return self.right.get_max()
+        return max_value
+
+        # if not self.right:
+        #     return
+        # else:
+        #     return self.right.get_max()
+
+    def iterative_get_max(self):
+        current_max = self.value
+        current = self
+        while current is not None:
+            if current.value > current_max:
+                current_max = current.value
+            current = current.right
+        return current_max
+
+    # Call the function `fn` on the value of each node
+    # recursive, depth-first traversal
+    # LIFO order of the tree elements
+    def for_each(self, fn):
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+        fn(self.value)
+
+    # iterative for each, depth-first traversal
+    def iterative_for_each(self, fn):
+        stack = []
+        # add the root node
+        stack.append(self)
+        # loop so long as the stack still has elements
+        while len(stack) > 0:
+            current = stack.pop()
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+            fn(current.value)
+
+    def breadth_first_for_each(self,fn):
+        queue = deque()
+
+        # add the root node
+        queue.append(self)
+
+        while len(queue) > 0:
+            current = queue.popleft()
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+
+            fn(current.value)
+
+    # Part 2 -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self, node):
+        # check current node for left side, if exists go left until reach a leaf 
+        if node.left:
+            node.left.in_order_print(node.left)
+            
+        # once done going left, print the current leaf
+        print(node.value)
+
+        # check current node for right side, if exists run that node exists run method
+        if node.right:
+            node.right.in_order_print(node.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        queue = []
+        queue.append(self)
+
+        while len(queue) > 0:
+            current = queue.pop(0)
+            print(current.value)
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        stack = []
+        stack.append(self)
+
+        while len(stack) > 0:
+            current = stack.pop()
+            print(current.value)
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+
+    # Stretch Goals -------------------------
+    # Note: Research may be required
+
+    # Print Pre-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import *
 
 start_time = time.time()
 
@@ -11,12 +12,36 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-
+# Polynomial run time
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+##############################################################
+# # ? run time
+# # loop through one list and see if it exists in other list
+# for name in names_1:
+#     if name in names_2:
+#         duplicates.append(name)
+
+# ##############################################################
+# logarithmic run time
+# create a binary search tree using the first list
+bst = BSTNode("name")
+for name in names_1:
+    bst.insert(name)
+
+# loop through second list, and check if bst contains each name
+for name in names_2:
+    if bst.contains(name) is True:
+        duplicates.append(name)
+
+##############################################################
+# # stretch
+# for each in set(names_1).intersection(names_2):
+#     duplicates.append(each)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
